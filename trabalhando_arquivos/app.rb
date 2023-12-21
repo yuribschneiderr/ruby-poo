ac3df1f731632760f2e5d6e95d67c6e07d340864
yuribschneiderr <yuribschneider@gmail.com>
2023-12-21 11:07:15 -0300

    aula 21-12

--- trabalhando_arquivos/app.rb
+++ trabalhando_arquivos/app.rb
@@ -1,5 +1,17 @@
-File.open('texte.txt', 'r') do |arq|
-	while line = arq.gets
-		puts line
+puts ARGV[0]
+puts ARGV[1]
+puts ARGV[2]
+puts ARGV.size
+
+puts "--------------"
+
+if ARGV.size > 0
+	File.open(ARGV[0], 'r') do |arq|
+		while line = arq.gets
+			puts line
+		end
 	end
+
+else
+	puts "Você deve informar o nome do arquivo a ser aberto. Ex: ruby app.rb texte.txt"
 end
