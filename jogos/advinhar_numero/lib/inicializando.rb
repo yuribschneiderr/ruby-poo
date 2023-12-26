e5ef1a68009d06aaee7ca583e3c5920ae407cf6e
yuribschneiderr <yuribschneider@gmail.com>
2023-12-26 15:57:36 -0300

    melhorando jogo em ruby

--- jogos/advinhar_numero/lib/inicializando.rb
+++ jogos/advinhar_numero/lib/inicializando.rb
@@ -1,13 +1,29 @@
+require 'tty-cursor'
+require 'tty-progressbar'
+
 class Inicializacao
 	def self.inicializando
-		print "Inicializando."
-		
-		4.times do |i|
-			sleep 1
-			print "."
+		system('clear')
+
+		bar = TTY::ProgressBar.new("Inicializando [:bar]", total: 50)
+		50.times do
+		  sleep(0.1)
+		  bar.advance  # by default increases by 1
 		end
 
-		puts " Inicialização completa!"
+		# cursor = TTY::Cursor
+		# print cursor.move_to(20, 10)
+
+		# print "Inicializando."
+		
+		# 4.times do |i|
+		# 	sleep 1
+		# 	print "."
+		# end
+
+		puts "Inicialização completa!"
 		sleep 1
+
+		system('clear')
 	end
 end
